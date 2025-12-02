108486073a28cd01f0e7c3d8d9f2b9dc50eda006
Arnold Paye <arnoldpayet@gmail.com>
2025-12-02 11:04:56 -0400

    defining typed state

---
+++ src/redux/reducers/cartReducer.ts
@@ -0,0 +1,15 @@
+export interface CartItem {
+  readonly id: number;
+  price: number;
+}
+
+export interface CartState {
+  items: CartItem[];
+  totalPrice: number;
+  rating?: number;
+}
+
+const initialState: CartState = {
+  items: [],
+  totalPrice: 0,
+};
