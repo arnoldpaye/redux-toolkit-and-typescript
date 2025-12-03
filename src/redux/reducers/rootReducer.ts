547bf8af8f8f5d181fc820d23b8c5a12f9372d94
Arnold Paye <arnoldpayet@gmail.com>
2025-12-03 10:00:43 -0400

    combining multiple reducers

---
+++ src/redux/reducers/rootReducer.ts
@@ -0,0 +1,12 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import cartReducer from "./cartReducer";
+import productsReducer from "./productsReducer";
+import userReducer from "./userReducer";
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  products: productsReducer,
+  user: userReducer,
+});
+
+export default rootReducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,27 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./reducers/cartReducer";
-import productsReducer from "./reducers/productsReducer";
-
-const preloadedState = {
-  cart: {
-    items: [
-      {
-        id: 1,
-        price: 50,
-        details: { manufacturer: "FFF", warranty: "1 year" },
-      },
-    ],
-    totalPrice: 50,
-  },
-};
+import rootReducer from "./reducers/rootReducer";
 
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    productsList: productsReducer,
-  },
-  devTools: process.env.NODE_ENV !== "production",
-  preloadedState,
+  reducer: rootReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
